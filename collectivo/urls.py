dedbd4f06ba6777c57594b5eff4f6518ce764450
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-10-19 15:32:23 +0200

    Automatic URL patterns

--- collectivo/urls.py
+++ collectivo/urls.py
@@ -1,18 +1,14 @@
 """URL patterns of the collectivo core."""
 from django.urls import path, re_path, include
 from django.conf import settings
-from django.contrib.staticfiles import views as staticviews
+from django.contrib.staticfiles.views import serve
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from collectivo import views
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView
-)
+
 
 app_name = 'collectivo'
 
-
 urlpatterns = [
-
     # Core API views
     path('api/collectivo/v1/version/',
          views.VersionView.as_view(), name='version'),
@@ -23,16 +19,17 @@ urlpatterns = [
     path('api/docs/',
          SpectacularSwaggerView.as_view(url_name='collectivo:api-schema'),
          name='api-docs'),
-
 ]
 
-# For development
+for app in settings.INSTALLED_APPS:
+    if app.startswith('collectivo.'):
+        pattern = path('', include(f'{app}.urls'))
+        urlpatterns.append(pattern)
+
 if settings.DEBUG:
 
     urlpatterns += [
         # Access static files
-        re_path(r'^static/(?P<path>.*)$', staticviews.serve),
+        re_path(r'^static/(?P<path>.*)$', serve),
 
-        # Test authentication
-        path('api/collectivo-dev/', include('collectivo.auth.urls')),
     ]

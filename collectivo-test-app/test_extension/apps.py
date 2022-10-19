b9950e0f9820a09586a6633814c6251abe56e1b1
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-10-19 15:36:32 +0200

    Register extension and microfrontend

--- collectivo-test-app/test_extension/apps.py
+++ collectivo-test-app/test_extension/apps.py
@@ -1,14 +1,38 @@
-"""Configuration file for the test_extension extension."""
-
+"""Configuration file for the test_extension app."""
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def post_migrate_callback(sender, **kwargs):
+    """Initialize extension after database is ready."""
+    from collectivo.extensions.utils import register_extension
+    from collectivo.ux.utils import register_microfrontend
+
+    register_extension(
+        name=sender.name,
+        version='0.0.1',
+        description='A test extension.'
+    )
+
+    register_microfrontend(
+        name=sender.name,
+        extension=sender.name,
+        path='http://localhost/static/test_extension/remoteEntry.js',
+        type='components'
+    )
 
 
 class TestExtensionConfig(AppConfig):
-    """Configuration class for the test_extension extension."""
+    """Configuration class for the test_extension app."""
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'test_extension'
 
     def ready(self):
-        """Import modules that should be loaded at start."""
-        from . import menus, urls  # noqa
+        """
+        Initialize app when it is ready.
+
+        Database calls are performed after migrations, using the post_migrate
+        signal. This signal only works if the app has a models.py module.
+        """
+        post_migrate.connect(post_migrate_callback, sender=self)

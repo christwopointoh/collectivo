350590062407e9dea6713116b805f5f409cd6789
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-11-01 14:59:45 +0100

    Register microfrontends and menuitems for members extension

--- collectivo/members/apps.py
+++ collectivo/members/apps.py
@@ -1,16 +1,55 @@
 """Configuration file for the members extension."""
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from collectivo.version import __version__
 
 
 def post_migrate_callback(sender, **kwargs):
     """Initialize extension after database is ready."""
     from collectivo.extensions.utils import register_extension
+    from collectivo.ux.utils import register_menuitem, register_microfrontend
+
+    name = 'members'
+    # TODO Language change
+    # TODO Error handling
+    # TODO Path handling
+    # TODO Test put and patch
+    # TODO Role filtering menu
 
     register_extension(
-        name=sender.name,
-        version='0.0.1',
-        description='This extension.'
+        name=name,
+        version=__version__,
+        description='An extension to manage member data and registration.'
+    )
+
+    register_microfrontend(
+        name='members_user_ui',
+        extension=name,
+        path='http://localhost:8001/hardcoded/path/remoteEntry.js',
+        type='modules'
+    )
+
+    register_microfrontend(
+        name='members_admin_ui',
+        extension=name,
+        path='http://localhost:8001/hardcoded/path/remoteEntry.js',
+        type='modules'
+    )
+
+    register_menuitem(
+        name='members_user_menu_item',
+        label='My membership',
+        extension=name,
+        menu='main_menu',
+        microfrontend='members_user_ui'
+    )
+
+    register_menuitem(
+        name='members_admin_menu_item',
+        label='Member data',
+        extension=name,
+        menu='main_menu',
+        microfrontend='members_admin_ui'
     )
 
 

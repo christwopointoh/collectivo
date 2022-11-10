93eb797e3fecf9f0a5e45af193e4c7ed0adc8748
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-11-10 18:29:28 +0100

    Update API docs

--- collectivo/dashboard/views.py
+++ collectivo/dashboard/views.py
@@ -6,7 +6,31 @@ from . import models, serializers
 
 
 class DashboardTileViewSet(viewsets.ModelViewSet):
-    """Manage dashboard tiles."""
+    """
+    Manage dashboard tiles.
+
+    List view requires authentication.
+    All other views require the role 'collectivo_admin'.
+
+    Dashboard tiles refer to webcomponents
+    that will be displayed in the dashboard.
+
+    Attributes:
+    - tile_id (CharField):
+      A unique name to identify the tile.
+    - extension (ForeignKey of Extension):
+      The extension that the menu belongs to.
+    - component_name (str):
+      Name of a registered component from the extensions' microfrontend.
+      The URL path after performing the action will be
+      '{base_url}/{extension}/{component name}'.
+    - order (FloatField):
+      Tiles will be sorted from lowest to highest order (default 1.0).
+    - required_role (CharField, optional):
+      If passed, only users with this role will see the tile.
+    - blocked_role (CharField, optional):
+      If passed, users with this role will not see the tile.
+    """
 
     def get_queryset(self):
         """Show only items where user has required roles."""

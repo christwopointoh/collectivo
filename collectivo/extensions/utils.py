53be4fe22f5f46d72ebac95bc72f3fae5531e718
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-11-10 11:19:06 +0100

    Improve extensions auth settings and error handling

--- collectivo/extensions/utils.py
+++ collectivo/extensions/utils.py
@@ -1,17 +1,19 @@
 """Utility functions of the extensions module."""
+import logging
 from collectivo.extensions.views import ExtensionViewSet
 from collectivo.utils import request
 from rest_framework.response import Response
 
 
+logger = logging.getLogger(__name__)
+
+
 def register_extension(name: str, **kwargs) -> Response:
     """Register an internal extension."""
-    # TODO Delete old stuff first?
-    # get = request(ExtensionViewSet, 'retrieve', kwargs, pk=name)
-    # if get.status_code == 200:
     kwargs['name'] = name
     request(ExtensionViewSet, 'destroy', kwargs, pk=name)
-    return request(ExtensionViewSet, 'create', kwargs, pk=name)
-    # else:
-    #     kwargs['name'] = name
-    #     return request(ExtensionViewSet, 'create', kwargs)
+    response = request(ExtensionViewSet, 'create', kwargs, pk=name)
+    if response.status_code != 201:
+        logger.debug(
+            f"Could not register extension '{name}': {response.content}")
+    return response

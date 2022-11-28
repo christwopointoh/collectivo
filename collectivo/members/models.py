b563e4061d124e97b0d01a48808ca41e3517a904
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-11-28 11:14:29 +0100

    Update member fields

--- collectivo/members/models.py
+++ collectivo/members/models.py
@@ -2,110 +2,127 @@
 from django.db import models
 
 
+class MemberTag(models.Model):
+    """A tag that can be assigned to members."""
+
+    name = models.CharField(max_length=255)
+    built_in = models.BooleanField(default=False)
+
+
+class MemberCard(models.Model):
+    """A membership card that can be assigned to members."""
+
+    active = models.BooleanField(default=False)
+
+
+class MemberAddon(models.Model):
+    """A person that can be assigned to members."""
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    person_type = models.CharField(
+        max_length=20,
+        choices=[
+            ('child', 'child'),
+            ('coshopper', 'coshopper'),
+        ]
+    )
+    birth_date = models.DateField(null=True)
+    membership_card = models.ForeignKey(
+        'MemberCard', null=True, on_delete=models.SET_NULL)
+
+
 class Member(models.Model):
     """A member of the collective."""
 
     # Account
     user_id = models.UUIDField(null=True, unique=True)
+    email = models.EmailField()
     email_verified = models.BooleanField(null=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
 
-    # Membership
-    membership_type = models.CharField(
-        help_text='Type of membership.',
+    # Personal data
+    person_type = models.CharField(
+        help_text='Type of person.',
         max_length=20,
-        default='active',
+        default='natural',
         choices=[
-            ('active', 'active'),
-            ('investing', 'investing'),
+            ('natural', 'natural'),
             ('legal', 'legal'),
         ]
     )
-    membership_status = models.CharField(
-        max_length=20,
-        help_text='Current status of membership.',
-        default='1_applicant',
-        choices=[
-            ('1_applicant', '1_applicant'),
-            ('2_provisional', '2_provisional'),
-            ('3_approved', '3_approved'),
-            ('4_normal', '4_normal'),
-            ('5_leaving', '5_leaving'),
-            ('6_left', '6_left'),
-            ('7_deceased', '7_deceased')
-        ]
-    )
-
-    # Coop shares
-    shares_number = models.IntegerField(default=0)
-    shares_payment_status = models.CharField(
-        max_length=20,
-        help_text='Status of payment.',
-        default='1_pending',
-        choices=[
-            ('1_pending', '1_pending'),
-            ('2_processing', '2_processing'),
-            ('3_success', '3_success'),
-            ('4_denied', '4_denied'),
-        ]
-    )
-    shares_payment_type = models.CharField(
-        max_length=20,
-        help_text='Type of payment.',
-        default='sepa',
-        choices=[
-            ('sepa', 'sepa'),
-            # ('transfer', 'transfer')
-        ]
-    )
-    shares_installment_plan = models.BooleanField(default=False)
-
-    # If payment type is not sepa
-    # bank_iban
-    # bank_blz
-    # bank_owner
-
-    # Personal data (real person)
-    title_pre = models.CharField(max_length=255, null=True)
-    title_post = models.CharField(max_length=255, null=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    # pronouns?
     gender = models.CharField(
         max_length=20,
-        null=True,
+        default='diverse',
         choices=[
             ('diverse', 'diverse'),
             ('female', 'female'),
             ('male', 'male'),
         ]
     )
-    date_birth = models.DateField(null=True)
     address_street = models.CharField(max_length=255, null=True)
     address_number = models.CharField(max_length=255, null=True)
-    address_is_home = models.BooleanField(null=True)
-    address_co = models.CharField(max_length=255, null=True)
     address_stair = models.CharField(max_length=255, null=True)
     address_door = models.CharField(max_length=255, null=True)
     address_postcode = models.CharField(max_length=255, null=True)
     address_city = models.CharField(max_length=255, null=True)
     address_country = models.CharField(max_length=255, null=True)
     phone = models.CharField(max_length=255, null=True)
-    phone_2 = models.CharField(max_length=255, null=True)
-    email = models.EmailField()  # Snyced with auth!
-    email_2 = models.EmailField(null=True)
-    homepage = models.CharField(max_length=255, null=True)
 
-    # Personal data (only for active members)
-    # Kinder/Miteinkäuferinnen/FamilienID
+    # FUTURE Personal data (only for active members)
+    # FUTURE kids = models.ManyToManyField('MemberAddon')
+    # FUTURE coshoppers = models.ManyToManyField('MemberAddon')
+
+    # Personal data (only for natural people)
+    birth_date = models.DateField(null=True)
 
     # Personal data (only for legal person)
     legal_name = models.CharField(max_length=255, null=True)
     legal_type = models.CharField(max_length=255, null=True)
-    legal_seat = models.CharField(max_length=255, null=True)
-    legal_type_id = models.CharField(max_length=255, null=True)
+    legal_id = models.CharField(max_length=255, null=True)
 
-    # To be done in serializer (not saved in django, but sent somewhere)
-    # Newsletter
+    # Membership
+    membership_started_date = models.DateField(null=True)
+    membership_cancelled_date = models.DateField(null=True)
+    membership_ended_date = models.DateField(null=True)
+    membership_status = models.CharField(
+        max_length=20,
+        help_text='Current status of membership.',
+        default='1_applicant',
+        choices=[
+            ('applicant', 'applicant'),
+            ('provisional', 'provisional'),
+            ('active', 'active'),
+            ('investing', 'investing'),
+            ('ended', 'ended'),
+        ]
+    )
+    membership_card = models.ForeignKey(
+        'MemberCard', null=True, on_delete=models.SET_NULL)
 
-    # To be done in frontend (does not have to be saved, just validated)
-    # Checkboxen
+    # Membership - Coop shares
+    shares_number = models.IntegerField(default=0)
+    shares_payment_date = models.DateField(null=True)
+    shares_payment_type = models.CharField(
+        max_length=20,
+        help_text='Type of payment.',
+        default='sepa',
+        choices=[
+            ('sepa', 'sepa'),
+            ('transfer', 'transfer')
+        ]
+    )
+    bank_account_iban = models.CharField(max_length=255, null=True)
+    bank_account_owner = models.CharField(max_length=255, null=True)
+    # FUTURE shares_installment_plan = models.BooleanField(default=False)
+
+    # Survey data
+    survey_first_heard = models.TextField(null=True)
+    survey_motivation = models.TextField(null=True)
+    survey_working_groups = models.TextField(null=True)
+    survey_skills = models.TextField(null=True)
+
+    # Other
+    tags = models.ManyToManyField('MemberTag')
+    admin_notes = models.TextField(null=True)

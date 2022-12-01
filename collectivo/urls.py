ebc0554c19bd210cfb1d2b37de8e82846e31f628
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-01 11:54:00 +0100

    Import url paths from all apps that start with collectivo

--- collectivo/urls.py
+++ collectivo/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
 ]
 
 for app in settings.INSTALLED_APPS:
-    if app.startswith('collectivo.'):
+    if app.startswith('collectivo'):
         pattern = path('', include(f'{app}.urls'))
         urlpatterns.append(pattern)
 

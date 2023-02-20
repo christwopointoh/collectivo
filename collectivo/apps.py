3147c531df360f37581476b6ec0e79a4fcf51316
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-02-20 10:25:40 +0100

    log version (#75)

--- collectivo/apps.py
+++ collectivo/apps.py
@@ -1,18 +1,22 @@
 """Configuration file for the collectivo package."""
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from logging import getLogger
+from collectivo.version import __version__
+
+logger = getLogger(__name__)
 
 
 def post_migrate_callback(sender, **kwargs):
     """Initialize package after database is ready."""
-    pass
+    logger.info(f"Collectivo v{__version__} is ready")
 
 
 class CollectivoConfig(AppConfig):
     """Configuration class of the collectivo package."""
 
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'collectivo'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "collectivo"
 
     def ready(self):
         """

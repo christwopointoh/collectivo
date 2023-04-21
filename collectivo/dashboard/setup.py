5025163facf7e63a300da211d3d5e92e70c7f600
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-04-21 13:32:58 +0200

    Add example data (#105)

--- collectivo/dashboard/setup.py
+++ collectivo/dashboard/setup.py
@@ -1,7 +1,11 @@
 """Setup function of the dashboard extension."""
+from django.conf import settings
+
 from collectivo.dashboard.apps import DashboardConfig
 from collectivo.extensions.models import Extension
 from collectivo.menus.models import MenuItem
+
+from .models import DashboardTile, DashboardTileButton
 
 
 def setup(sender, **kwargs):
@@ -33,3 +37,35 @@ def setup(sender, **kwargs):
         parent="admin",
         order=80,
     )
+
+    if settings.COLLECTIVO["dev.create_test_data"]:
+        DashboardTile.register(
+            name="welcome_tile",
+            label="Welcome to your platform!",
+            extension=extension,
+            source="db",
+            order=0,
+            content=(
+                "Welcome {{ user.first_name }} {{ user.last_name }}. You can"
+                " edit the content of this tile under the admin settings for"
+                " the dashboard."
+            ),
+        )
+
+        for i in range(1, 4, 1):
+            button = DashboardTileButton.objects.register(
+                name=f"example_button_{i}",
+                label=f"Example Button {i}",
+            )
+
+            tile = DashboardTile.register(
+                name=f"example_tile_{i}",
+                label=f"Example Tile_{i}",
+                extension=extension,
+                source="db",
+                content=(
+                    "This is a custom tile. You can add as many as you want."
+                ),
+            )
+
+            tile.buttons.set([button])

a03f0a7c4d2eb9078a5e36379d551010be04fb8c
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-07-12 16:19:43 +0200

    Fix namespace and tests

---
+++ collectivo/auth/__init__.py
@@ -0,0 +1,1 @@
+"""Namespace for authentication-related extensions."""
--- collectivo/auth/keycloak/signals.py
+++ collectivo/auth/keycloak/signals.py
@@ -2,6 +2,7 @@
 from django.contrib.auth import get_user_model
 from django.db.models import signals
 
+from .api import KeycloakAPI
 from .models import KeycloakUser
 
 
@@ -13,9 +14,23 @@ def update_keycloak_user(sender, instance, created, **kwargs):
         KeycloakUser.objects.create(user=instance)
 
 
+def delete_keycloak_user(sender, instance, *args, **kwargs):
+    """Delete model and synchronize with keycloak."""
+    keycloak = KeycloakAPI()
+    uuid = keycloak.get_user_id(instance.email)
+    keycloak.delete_user(uuid)
+
+
 signals.post_save.connect(
     update_keycloak_user,
     sender=get_user_model(),
     dispatch_uid="update_keycloak_user",
+    weak=False,
+)
+
+signals.post_delete.connect(
+    delete_keycloak_user,
+    sender=get_user_model(),
+    dispatch_uid="delete_keycloak_user",
     weak=False,
 )

72d24a44eaeba00a1e10bd770edf6918e38fe088
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-07-13 14:33:48 +0200

    Change logic

--- collectivo/shifts/__init__.py
+++ collectivo/shifts/__init__.py
@@ -3,14 +3,14 @@
 
 from os.path import abspath, dirname
 
-from django.apps import apps
+from django.conf import settings
 
 
 def load_tests(loader, tests, pattern):
     """Load tests only if app is installed."""
     from django.conf import settings
 
-    if apps.is_installed("collectivo.shifts"):
+    if "collectivo.shifts" in settings.INSTALLED_APPS:
         return loader.discover(
             start_dir=dirname(abspath(__file__)), pattern=pattern
         )

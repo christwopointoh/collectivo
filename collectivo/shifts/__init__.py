a62a9fc3997d1ce1cd4b23b6f065d8ad476973f6
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-07-13 14:28:07 +0200

    Run shift tests only if app is installed

--- collectivo/shifts/__init__.py
+++ collectivo/shifts/__init__.py
@@ -1,1 +1,16 @@
 """Shifts extension for collectivo."""
+
+
+from os.path import abspath, dirname
+
+from django.apps import apps
+
+
+def load_tests(loader, tests, pattern):
+    """Load tests only if app is installed."""
+    from django.conf import settings
+
+    if apps.is_installed("collectivo.shifts"):
+        return loader.discover(
+            start_dir=dirname(abspath(__file__)), pattern=pattern
+        )
